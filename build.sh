46596c6b0f1a6142006afb92a7ff039aaf41e977
NyxCode <nyxcode.development@gmail.com>
2018-07-09 13:31:17 +0200

    update buildscript & todo

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+echo "deleting existing artifacts..."
 rm -rf build/libs/
 rm blinded-backend.jar
+
+echo "updating repository..."
 git pull
+
+echo "building..."
 ./gradlew shadowJar
+
+echo "copying artifacts..."
 mv build/libs/blinded-backend.jar .
